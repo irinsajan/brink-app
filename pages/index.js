02be1a229b5bd4355fe4f9278c1e128992e66c58
irinsajan <irinsajan@gmail.com>
2022-07-10 11:53:16 +0200

    styled 5 sections

--- pages/index.js
+++ pages/index.js
@@ -2,208 +2,113 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="container">
+    <div>
       <Head>
-        <title>Create Next App</title>
+        <title>The Brink App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <main>  
+        {/* heading section */}      
+        <header>
+          <div className="container-fluid">
+              <div className="header">
+                <p className="paragraph1">Luctus vitae</p>
+                <h1 className="heading1">Utrum ut placerat nec, varius sit amet lacus.</h1>
+              </div>
+          </div>
+        </header>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        {/* 2 image hero section */}
+        <section>
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-md-6 ps-0"><img className="hero-img" src="https://via.placeholder.com/700x500/5F35F7"/></div>
+              <div className="col-md-6 pe-0"><img className="hero-img" src="https://via.placeholder.com/700x500/5F35F7"/></div>
+            </div>
+          </div>
+        </section>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        {/* text section */}
+        <section>
+          <div className="container">
+            <div className="row justify-content-center mt-5 mb-4">
+              <div className="col-md-4">
+                <h2 className="heading2 text-center">
+                  Suspendisse tincidunt vulputate ex, a laoreet neque luctus vitae.
+                </h2>
+              </div>
+            </div>
+            <div className="row justify-content-center mb-5">
+              <div className="col-md-5">
+                <p className="paragraph2">
+                  Suspendisse tincidunt vulputate ex, a laoreet neque luctus vitae. Quisque maximus ante sapien, at 
+                  phareta sam sagittis eu. Maecenas nibh leo, interdum ac malesuada quis, tempus vel lacus.
+                </p>
+                <p className="paragraph2">
+                  Aliquam quis velit et nunc scelerisque auctor quis id lacus. Proin sem turpis, rutrum ut placerat
+                  nec, varius sit amet lacus, Praesent sed viverra lorem.
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+        {/* gallery section */}
+        <section>
+          <div className="container">
+            <div className="row mb-4">
+              <div className="col-md-4">
+                <div className="row mb-3">
+                  <img className="hero-img" src="https://via.placeholder.com/700x500/5F35F7"/>
+                </div>
+                <div className="row mb-3">
+                  <img className="hero-img" src="https://via.placeholder.com/700x500/5F35F7"/>
+                </div>
+              </div>
+              <div className="col-md-4">                
+                  <img className="hero-img" src="https://via.placeholder.com/700x1030/5F35F7"/>                
+              </div>
+              <div className="col-md-4">
+                <div className="row mb-3">
+                  <img className="hero-img" src="https://via.placeholder.com/700x500/5F35F7"/>
+                </div>
+                <div className="row mb-3">
+                  <img className="hero-img" src="https://via.placeholder.com/700x500/5F35F7"/>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+        {/* black background text section */}
+        <section>
+          <div className="container">
+            <div className="row bg1 pt-5">
+              <div className="col-md-6">
+                <h2 className="heading3 pe-5">
+                  Proin enim enim, tincidunt consequat sollicitudin.
+                </h2>
+              </div>
+              <div className="col-md-6">
+                <p className="paragraph3 ps-5">
+                  Aliquam vel orci pulvinar, porta velit in, volutpat neque. Nam vel leo nec nibh 
+                  tempus congue. Morbi rhoncus bibendum enim sit amet pulvinar. Quisque lobortis commodo 
+                  nunc, a rhoncus diam aliquam ac. Pellentesque non augue ullamcorper arcu facilisis bibendum. 
+                  Nulla quis tincidunt turpis. Sed quis erat arcu. 
+                </p>
+                <p className="paragraph3 ps-5">
+                  Phasellus gravida felis quis ex mattis, sed suscipit tellus tincidunt. In imperdiet dapibus 
+                  vehicula. In auctor nec nibh vestibulum ornare. Morbi porta, enim hendrerit consectetur ultrices, 
+                  libero dui ultricies libero, vel rutrum sem magna quis augue. Nulla quis magna at lorem sodales gravida.
+                </p>
+              </div>              
+            </div>
+          </div>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+        </section>
+       
+      </main>    
     </div>
   )
 }
