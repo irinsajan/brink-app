4509526df692f27b5a59b36a70cb48a6743d1ce2
irinsajan <irinsajan@gmail.com>
2022-07-11 11:50:00 +0200

    changed order in mobile devices

--- pages/index.js
+++ pages/index.js
@@ -22,11 +22,24 @@ export default function Home() {
         {/* 2 image hero section */}
         <section>
           <div className="container-fluid">
-            <div className="row">
+            <div className="row d-none d-md-flex">
               <div className="col-md-6 ps-0 mb-3 col-img1"><img className="hero-img" src="https://via.placeholder.com/700x500/5F35F7"/></div>
               <div className="col-md-6 pe-0 mb-3 col-img2"><img className="hero-img" src="https://via.placeholder.com/700x500/5F35F7"/></div>
             </div>
+          
+
+            <div id="carouselExampleSlidesOnly" class="carousel slide d-block d-md-none" data-ride="carousel">
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img class="d-block hero-img" src="https://via.placeholder.com/700x500/5F35F7" />
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block hero-img" src="https://via.placeholder.com/700x500/5F35F7" />
+                </div>
+              </div>
+            </div>
           </div>
+
         </section>
 
         {/* text section */}
@@ -85,42 +98,38 @@ export default function Home() {
         <section>
           <div className="container-fluid bg1">
             <div className="row pt-5">
-              <div className="col-md-6">
+              <div className="order-md-1 col-md-6">
                 <h2 className="heading3 pe-5">
                   Proin enim enim, tincidunt consequat sollicitudin.
                 </h2>
               </div>
-              <div className="col-md-6">
+              <div className="order-md-2 col-md-6">
                 <p className="paragraph3">
                   Aliquam vel orci pulvinar, porta velit in, volutpat neque. Nam vel leo nec nibh 
                   tempus congue. Morbi rhoncus bibendum enim sit amet pulvinar. Quisque lobortis commodo 
                   nunc, a rhoncus diam aliquam ac. Pellentesque non augue ullamcorper arcu facilisis bibendum. 
                   Nulla quis tincidunt turpis. Sed quis erat arcu. 
                 </p>
+              </div>
+              <div className="order-md-5 col-md-8">                
+                  <img className="hero-img" src="https://via.placeholder.com/700x400/5F35F7"/>                
+              </div>
+              <div className="order-md-3 col-md-6">
+              </div>
+              <div className="order-md-4 col-md-6">
                 <p className="paragraph3">
                   Phasellus gravida felis quis ex mattis, sed suscipit tellus tincidunt. In imperdiet dapibus 
                   vehicula. In auctor nec nibh vestibulum ornare. Morbi porta, enim hendrerit consectetur ultrices, 
                   libero dui ultricies libero, vel rutrum sem magna quis augue. Nulla quis magna at lorem sodales gravida.
                 </p>
-              </div> 
-            </div> 
-          </div>
-        </section>
-
-
-        {/* black background overlap images */}
-        <section>
-          <div className="container-fluid bg1">
-            <div className="row">
-              <div className="col-md-8">                
-                  <img className="hero-img" src="https://via.placeholder.com/700x400/5F35F7"/>                
-              </div>
-              <div className="col-md-8 ms-auto overlap">                
+              </div>               
+              <div className="order-md-last col-md-8 ms-auto overlap">                
                   <img className="hero-img" src="https://via.placeholder.com/700x400/4425B2"/>                
               </div>
-            </div>            
+            </div>
           </div>
         </section>
+          
        
 
         {/* footer section */}
